165139d46a74c6955f91a8b2e3f0ab59e6bdf0f5
Muhammedrameesz <rameezta555@gmail.com>
2025-01-22 22:01:59 +0530

    new

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import Underline from "../ui/Underline";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import FaqImage from "../assets/images/FAQ Background removed.png"
+import FaqImage from "../assets/images/FaqImage.png"
 
 export default function StyledAccordion() {
   const [expanded, setExpanded] = useState(0);
